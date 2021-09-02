64d250f25132e2c98235ae1ee7248496502e4372
MDorsaz <michel.dorsaz@students.hevs.ch>
2021-09-02 15:09:03 +0300

    Friend list

---
+++ src/main/java/com/example/HelloThymeleaf/domain/Friend.java
@@ -0,0 +1,23 @@
+package com.example.HelloThymeleaf.domain;
+
+public class Friend {
+	public String name;
+	
+	public Friend(String name) {
+		this.name = name;
+	}
+
+	public Friend() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
--- src/main/java/com/example/HelloThymeleaf/web/HelloController.java
+++ src/main/java/com/example/HelloThymeleaf/web/HelloController.java
@@ -1,9 +1,17 @@
 package com.example.HelloThymeleaf.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.HelloThymeleaf.domain.Friend;
+import com.example.HelloThymeleaf.domain.Student;
 
 @Controller
 public class HelloController {
@@ -17,4 +25,27 @@ public class HelloController {
 		model.addAttribute("age", age);
 		return "hello";
 	}
+	
+	
+	static ArrayList<Friend> friendList = new ArrayList<Friend>();
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("friend", new Friend());
+		return "friendsForm";
+	}
+	
+	@PostMapping("/index")
+	public String formSubmit(
+			@ModelAttribute Friend newFriend, 
+			Model model) {	
+			
+		friendList.add(newFriend);
+		
+		model.addAttribute("friends", friendList);
+		
+		return "friendsForm";
+	}
+	
+	
 }
